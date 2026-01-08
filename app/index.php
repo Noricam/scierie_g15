92d2cfe6591e6ff8019cb04fc4ba67b4f4113292
Noricam <karaoke3099@duck.com>
2026-01-08 13:11:17 +0100

    (Debug) fix css

--- app/index.php
+++ app/index.php
@@ -12,8 +12,8 @@ require __DIR__ . '/bootstrap.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="index">
 	
-	<link rel="stylesheet" href="style.css">
-	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/style.css">
+	<link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
 </head>
 
 <body>	
