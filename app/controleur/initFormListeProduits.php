77673c4d9e9c677d4888b53d5f0fd8ed65ff9dd9
Noricam <karaoke3099@duck.com>
2026-01-08 15:09:47 +0100

    (fix) produits

--- app/controleur/initFormListeProduits.php
+++ app/controleur/initFormListeProduits.php
@@ -27,7 +27,7 @@ for ($i = 0; $i < count($produits); $i++) {
     $produit .= "<li class='sub-item'><p class='texte'>" . htmlspecialchars($produits[$i]->getDescr(), ENT_QUOTES, 'UTF-8') . "</p></li>";
     
     // Protection XSS sur le nom de l'image
-    $produit .= "<li class='sub-item'><img class='image' alt=" . htmlspecialchars($produits[$i]->getTitre() . " src='/images/" . htmlspecialchars($produits[$i]->getImg(), ENT_QUOTES, 'UTF-8') . "'></li>";        
+    $produit .= "<li class='sub-item'><img class='image' alt=" . htmlspecialchars($produits[$i]->getTitre(), ENT_QUOTES, 'UTF-8') . " src='/images/" . htmlspecialchars($produits[$i]->getImg(), ENT_QUOTES, 'UTF-8') . "'></li>";        
     
     $produit .= "</ul></li></ul>";
     
