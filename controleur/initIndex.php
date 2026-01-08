311e6c04e2b736f6ab6de2d3ce20cee5739731f5
charles <84155265+nqkoz@users.noreply.github.com>
2026-01-08 11:25:35 +0100

    protection contre les injections SQL et les XSS

--- controleur/initIndex.php
+++ controleur/initIndex.php
@@ -1,52 +1,43 @@
 <?php
+    /* Connexion à la bdd */
+    $con = mysqli_connect("localhost", "root", "", "scierie");
 
-	/* Connexion à la bdd */
-	$con = mysqli_connect("localhost", "scierie", "dVR&6jsY3391DYBaUysK", "scierie");
+    if (mysqli_connect_errno()){
+        echo "Erreur de connexion: " . mysqli_connect_error();
+    }
 
-	/* Gestion des erreurs de connexion */
-	if (mysqli_connect_errno()){
-		echo "Erreur de connexion: " . mysqli_connect_error();
-	}
+    mysqli_set_charset($con,"utf8");
 
-	mysqli_set_charset($con,"utf8");
+    /* Requête SQL */
+    $sql = "SELECT home.titre, home.descr, home.img FROM home";
+    $requete = $con->query($sql);
 
-	/* Requête SQL */
-	$sql = "SELECT home.titre, home.descr, home.img FROM home";
-
-	/* Gestion des erreurs de requête sql */
-	if (!mysqli_query($con, $sql)){
-		echo "Création échouée" . mysqli_error($con);
-	}
-
-	$requete = $con->query($sql);
-
-	while ($resultat = mysqli_fetch_array($requete))
+    while ($resultat = mysqli_fetch_array($requete))
     {
-		$description = "<ul class='main-list'>";
-		if($resultat['titre']!='') {
-			$description .= "<li class='main-item'><p class='titre'>".$resultat['titre']."</p></li>";
-		}
-		if($resultat['descr']!='' && $resultat['img']!=''){
-			$description .= "<li class ='main-item'><ul class ='sub-list'>";
+        $description = "<ul class='main-list'>";
+        
+        // Protection XSS avec htmlspecialchars sur le titre
+        if($resultat['titre']!='') {
+            $description .= "<li class='main-item'><p class='titre'>".htmlspecialchars($resultat['titre'], ENT_QUOTES, 'UTF-8')."</p></li>";
+        }
 
-			$description .= "<li class='sub-item'><p class='texte'>".$resultat['descr']."</p></li>";
-
-			$description .= "<li class='sub-item'><img class='image' src='images/".$resultat['img']."'></li>";
-			
-			$description .= "</ul></li>";
-		
-		}else{
-			if($resultat['descr']!=''){
-				$description .= "<li class='main-item'><p class='texte'>".$resultat['descr']."</p></li>";
-			}
-			if($resultat['img']!=''){
-				$description .= "<li class='main-item'><img class='image' src='images/".$resultat['img']."'></li>";
-			}
-		}
-		$description .="</ul>";
+        if($resultat['descr']!='' && $resultat['img']!=''){
+            $description .= "<li class ='main-item'><ul class ='sub-list'>";
+            // Protection XSS sur la description et le nom de l'image
+            $description .= "<li class='sub-item'><p class='texte'>".htmlspecialchars($resultat['descr'], ENT_QUOTES, 'UTF-8')."</p></li>";
+            $description .= "<li class='sub-item'><img class='image' src='images/".htmlspecialchars($resultat['img'], ENT_QUOTES, 'UTF-8')."'></li>";
+            $description .= "</ul></li>";
+        } else {
+            if($resultat['descr']!=''){
+                $description .= "<li class='main-item'><p class='texte'>".htmlspecialchars($resultat['descr'], ENT_QUOTES, 'UTF-8')."</p></li>";
+            }
+            if($resultat['img']!=''){
+                $description .= "<li class='main-item'><img class='image' src='images/".htmlspecialchars($resultat['img'], ENT_QUOTES, 'UTF-8')."'></li>";
+            }
+        }
+        $description .="</ul>";
         echo $description;
+    }
 
-	}
-
-	mysqli_close($con)
+    mysqli_close($con);
 ?>
--- controleur/updateDescription.php
+++ controleur/updateDescription.php
@@ -2,27 +2,21 @@
 session_start();
 require("../metier/DB_connector.php");
 
-// Vérification de la session pour s'assurer que l'utilisateur est autorisé
+// Vérification de sécurité : seul un admin connecté peut modifier
 if (!isset($_SESSION['id'])) {
     header("Location: ../connexion.php");
     exit();
 }
 
-// Vérification de la présence de la donnée à mettre à jour
 if (isset($_POST["areaModifAccueil"])) {
-    
     try {
-        // Utilisation du connecteur existant du projet 
         $cnx = new DB_Connector();
         $jeton = $cnx->openConnexion();
 
-        // Requête préparée pour sécuriser l'entrée utilisateur 
-        // Note : Dans scierie.sql, la table est 'home' et la colonne est 'descr'
+        // Requête préparée pour éviter les injections SQL
         $req = "UPDATE home SET descr = ? WHERE id = 1";
-        
         $stmt = $jeton->prepare($req);
         
-        // Exécution sécurisée : la donnée de $_POST n'est jamais concaténée directement
         if ($stmt->execute([$_POST["areaModifAccueil"]])) {
             $_SESSION['msgModifOk'] = "Description mise à jour avec succès.";
         } else {
@@ -30,14 +24,11 @@ if (isset($_POST["areaModifAccueil"])) {
         }
 
         $cnx->closeConnexion();
-        
     } catch (Exception $e) {
-        // En production, il vaut mieux logger l'erreur plutôt que de l'afficher
         $_SESSION['msgModifNok'] = "Erreur système lors de la modification.";
     }
 }
 
-// Redirection vers la page d'accueil ou d'administration
 header('Location: ../index.php'); 
 exit();
 ?>
