0ee18aa3e640211c1eff7a4844c219560ecdf12c
charles <84155265+nqkoz@users.noreply.github.com>
2026-01-08 13:52:27 +0100

    blocage de 30 sec après 3 essais

--- app/controleur/traitementFormConnexion.php
+++ app/controleur/traitementFormConnexion.php
@@ -1,34 +1,59 @@
-﻿<?php
+<?php
 session_start();
 require("../metier/DB_connector.php");
 require("../metier/User.php");
 require("../Dao/UserDao.php");
 
-// On récupère et test l'existance des variables de connexion
-if (isset($_GET['idUtil']) || isset($_GET['mdpUtil'])) {
+// Initialisation des variables de tentative si elles n'existent pas
+if (!isset($_SESSION['tentatives'])) {
+    $_SESSION['tentatives'] = 0;
+}
 
-    // Accès à la BDD
+// Vérification du blocage (ex: blocage de 30 secondes pour le challenge)
+if (isset($_SESSION['expire_blocage']) && time() < $_SESSION['expire_blocage']) {
+    $attente = $_SESSION['expire_blocage'] - time();
+    $_SESSION['errCnx'] = "Trop d'échecs. Veuillez patienter " . $attente . " secondes.";
+    header('Location:../connexion.php');
+    exit();
+}
+
+if (isset($_GET['idUtil']) && isset($_GET['mdpUtil'])) {
+
     $cnx = new DB_Connector();
     $jeton = $cnx->openConnexion();
-
     $userManager = new UserDao($jeton);
+    
     $userId = $_GET['idUtil'];
     $mdp = $_GET['mdpUtil'];
 	
+    // Note: On utilise MD5 car c'est le format actuel de votre BDD 
     $jetonExistance = $userManager->userExist($userId, MD5($mdp));
+
     if ($jetonExistance) {
-      $_SESSION['id'] = $userId;
+        // Succès : on réinitialise le compteur
+        $_SESSION['id'] = $userId;
+        $_SESSION['tentatives'] = 0;
+        unset($_SESSION['expire_blocage']);
          
-		  $cnx->closeConnexion();
-		  header('Location:../index.php');
+        $cnx->closeConnexion();
+        header('Location:../index.php');
+        exit();
     } else {
-		  $_SESSION['errCnx'] = "La combinaison nom d'utilisateur/mot de passe est incorrecte"; 
-		  $cnx->closeConnexion();
-		  header('Location:../connexion.php');	
+        // Échec : on incrémente le compteur
+        $_SESSION['tentatives']++;
+
+        if ($_SESSION['tentatives'] >= 3) {
+            // Blocage de 30 secondes après 3 échecs
+            $_SESSION['expire_blocage'] = time() + 30;
+            $_SESSION['errCnx'] = "Compte bloqué pour 30 secondes (3 échecs).";
+        } else {
+            $restant = 3 - $_SESSION['tentatives'];
+            $_SESSION['errCnx'] = "Identifiants incorrects. Tentatives restantes : " . $restant; 
+        }
+
+        $cnx->closeConnexion();
+        header('Location:../connexion.php');
+        exit();
     }
 }
-
-
-
-
 ?>
