465f2a3b5a4324c8337b806871ee0ac438029d00
lavision1 <salimdiallo786@gmail.com>
2026-01-08 11:29:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,8 @@ session_start();
 <title>TEST GREEN IT</title>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+	<meta name="description" content="index">
+	
 	<link rel="stylesheet" href="style.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
