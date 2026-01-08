17158521f9f78696b788b143bcdfcc25df43895e
lavision1 <salimdiallo786@gmail.com>
2026-01-08 11:31:59 +0100

    Update video.php

--- video.php
+++ video.php
@@ -8,6 +8,7 @@ session_start();
 		<title>TRUC</title>
 		<meta charset="utf-8">
   		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="video">
 		<link rel="stylesheet" href="style.css">
 	</head>
 
