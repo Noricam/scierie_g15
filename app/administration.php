70ff4298fe74af8dc1891c1d2e49829359a24a39
lavision1 <salimdiallo786@gmail.com>
2026-01-08 13:50:44 +0100

    Update administration.php

--- app/administration.php
+++ app/administration.php
@@ -214,7 +214,7 @@ if ((!(isset($_SESSION['id'])) || empty($_SESSION['id'])) && $_SESSION['role'] !
 <footer id="footer">
 	<ul class="footer-links">
     	<li class="footer-item">©Projet 3iL</li>
-    	<li class="footer-item"><a href="https://www.facebook.com/Scierie-du-Fargal-613509152159633/" target="_blank"><img id="logo" src="/images/facebook.png" loading="lazy" decoding="async"></a></li>
+    	<li class="footer-item"><a href="https://www.facebook.com/Scierie-du-Fargal-613509152159633/" target="_blank"><img id="logo" src="/images/facebook.png" alt="Logo facebook" loading="lazy" decoding="async"></a></li>
     	<li class="footer-item">Site test</li>
 	</ul>
 </footer>
