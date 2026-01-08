2951b5d0da676d7d0e80a43c540166476c537d1c
lavision1 <salimdiallo786@gmail.com>
2026-01-08 13:41:35 +0100

    Update index.php

--- app/index.php
+++ app/index.php
@@ -84,7 +84,7 @@ require __DIR__ . '/bootstrap.php';
 <footer id="footer">
 	<ul class="footer-links">
     	<li class="footer-item">©Projet 3iL</li>
-    	<li class="footer-item"><a href="#" target="_blank"><img id="logo" src="/images/facebook.png" loading="lazy" decoding="async"></a></li>
+    	<li class="footer-item"><a href="https://www.facebook.com/Scierie-du-Fargal-613509152159633/" target="_blank"><img id="logo" src="/images/facebook.png" loading="lazy" decoding="async"></a></li>
     	<li class="footer-item">Site test</li>
 	</ul>
 </footer>
