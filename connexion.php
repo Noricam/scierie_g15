b9695bab3f898fa6b8486b041cbe6af489f2a89d
lavision1 <salimdiallo786@gmail.com>
2026-01-08 10:11:38 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -6,6 +6,9 @@ session_start();
 <html lang="fr">
 
 <head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<title>TEST GREEN IT</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="style.css">
