8caf7dfe9cfd1cd04bc32cb104a1e64677e2d9ff
Noricam <karaoke3099@duck.com>
2026-01-08 09:50:50 +0100

    (Fix) Balises <ul> footer

--- administration.php
+++ administration.php
@@ -194,11 +194,11 @@ if ((!(isset($_SESSION['id'])) || empty($_SESSION['id'])) && $_SESSION['role'] !
 	</div>
 <!--*************** PIED DE PAGE ***************-->
 <footer id="footer">
-<ul class="footer-links">
-    <li class="footer-item">©Projet 3iL</li>
-    <li class="footer-item"><a href="#" target="_blank"><img id="logo" src="images/facebook.png"></a></li>
-    <li class="footer-item">Site test</li>
-<ul/>
+	<ul class="footer-links">
+    	<li class="footer-item">©Projet 3iL</li>
+    	<li class="footer-item"><a href="#" target="_blank"><img id="logo" src="images/facebook.png"></a></li>
+    	<li class="footer-item">Site test</li>
+	</ul>
 </footer>
 <!--*************** PIED DE PAGE ***************-->
 	
--- produits.php
+++ produits.php
@@ -57,11 +57,11 @@ session_start();
 	
 <!--*************** PIED DE PAGE ***************-->
 <footer id="footer">
-<ul class="footer-links">
-    <li class="footer-item">©Projet 3iL</li>
-    <li class="footer-item"><a href="#" target="_blank"><img id="logo" src="images/facebook.png"></a></li>
-    <li class="footer-item">Site test</li>
-<ul/>
+	<ul class="footer-links">
+    	<li class="footer-item">©Projet 3iL</li>
+    	<li class="footer-item"><a href="#" target="_blank"><img id="logo" src="images/facebook.png"></a></li>
+    	<li class="footer-item">Site test</li>
+	</ul>
 </footer>
 <!--*************** PIED DE PAGE ***************-->
 	<script src="scripts/initListeProduits.js"></script>
