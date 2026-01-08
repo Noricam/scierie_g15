0cd32b7e08b0dbe4b000ad85a92d8a1191b1e6b9
Noricam <karaoke3099@duck.com>
2026-01-08 14:49:15 +0100

    (fix) alt images

--- app/controleur/initIndex.php
+++ app/controleur/initIndex.php
@@ -31,14 +31,14 @@
         if($resultat['descr'] != '' && $resultat['img'] != ''){
             $description .= "<li class ='main-item'><ul class ='sub-list'>";
             $description .= "<li class='sub-item'><p class='texte'>" . htmlspecialchars($resultat['descr'], ENT_QUOTES, 'UTF-8') . "</p></li>";
-            $description .= "<li class='sub-item'><img class='image' src='/images/" alt="image" . htmlspecialchars($resultat['img'], ENT_QUOTES, 'UTF-8') . "'></li>";
+            $description .= "<li class='sub-item'><img class='image' src='/images/" . htmlspecialchars($resultat['img'], ENT_QUOTES, 'UTF-8') . "'></li>";
             $description .= "</ul></li>";
         } else {
             if($resultat['descr'] != ''){
                 $description .= "<li class='main-item'><p class='texte'>" . htmlspecialchars($resultat['descr'], ENT_QUOTES, 'UTF-8') . "</p></li>";
             }
             if($resultat['img'] != ''){
-                $description .= "<li class='main-item'><img class='image' src='/images/" alt="images" . htmlspecialchars($resultat['img'], ENT_QUOTES, 'UTF-8') . "'></li>";
+                $description .= "<li class='main-item'><img class='image' alt=" . htmlspecialchars($resultat['titre'], ENT_QUOTES, 'UTF-8') . " src='/images/" . htmlspecialchars($resultat['img'], ENT_QUOTES, 'UTF-8') . "'></li>";
             }
         }
         $description .= "</ul>";
