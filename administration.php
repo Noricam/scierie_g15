8d01873b52e839f286491d1715c33a741494ff10
lavision1 <salimdiallo786@gmail.com>
2026-01-08 10:10:32 +0100

    Update administration.php

--- administration.php
+++ administration.php
@@ -9,6 +9,9 @@ if ((!(isset($_SESSION['id'])) || empty($_SESSION['id'])) && $_SESSION['role'] !
 
 <html lang="fr">
 <head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 	<title>TEST GREEN IT</title>
 
 	<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1">
