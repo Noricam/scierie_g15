115ae7a86e7764979f97c878b5fb3be4a6fc1ae1
Louan DESHAYES <louandeshayes@gmail.com>
2026-01-08 12:56:33 +0100

    Change Bootstrap CSS link to local file

--- contact.php
+++ contact.php
@@ -12,7 +12,7 @@ session_start();
 		<title>TRUC</title>
 
 		<link rel="stylesheet" href="style.css">
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 
 	</head>
 
